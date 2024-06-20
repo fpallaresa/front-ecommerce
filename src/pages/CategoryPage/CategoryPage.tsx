65ff228f99ca85a6f718e3fa7b085355615f3671
francesc <you@example.com>
2024-06-20 18:54:30 +0200

    added API categories call Header and move header and footer structure on APP

--- src/pages/CategoryPage/CategoryPage.tsx
+++ src/pages/CategoryPage/CategoryPage.tsx
@@ -1,5 +1,4 @@
 import "../CategoryPage/CategoryPage.scss";
-import Header from "../../components/Header/Header";
 import CategoryPageGrid from "./CategoryPageGrid/CategoryPageGrid";
 import BreadcrumbsCategory from "./BreadcrumbsCategory/BreadcrumbsCategory";
 import CategoryPaginator from "./CategoryPaginator/CategoryPaginator";
@@ -7,7 +6,6 @@ import CategoryPaginator from "./CategoryPaginator/CategoryPaginator";
 const CategoryPage = (): JSX.Element => {
   return (
     <div className="category-page">
-      <Header></Header>
       <BreadcrumbsCategory></BreadcrumbsCategory>
       <CategoryPageGrid></CategoryPageGrid>
       <CategoryPaginator></CategoryPaginator>
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,10 @@
 import "../HomePage/HomePage.scss";
-import Header from "../../components/Header/Header";
 import CategoryGridHome from "./CategoryGridHome/CategoryGridHome";
 import SliderHome from "./SliderHome/SliderHome";
 
 const HomePage = (): JSX.Element => {
   return (
     <div className="home-page">
-      <Header></Header>
       <SliderHome></SliderHome>
       <CategoryGridHome></CategoryGridHome>
     </div>
