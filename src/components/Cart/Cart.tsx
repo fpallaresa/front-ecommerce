fce4068331f9af3809808500db5f2e06f55f7770
francesc <you@example.com>
2024-06-27 09:23:26 +0200

    added any css changes on cart window

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "../Cart/Cart.scss";
-import close from "../../assets/close.svg";
-import product from "../../assets/dummy.png"
+import closeBlack from "../../assets/close-black.svg";
+import product from "../../assets/dummy.png";
+
 interface cartProps {
   onClose: () => void;
 }
@@ -9,27 +10,33 @@ const Cart: React.FC<cartProps> = ({ onClose }): JSX.Element => {
   return (
     <div className="cart">
       <div className="cart__header">
-        <img src={close} className="cart__close-icon" alt="Close" onClick={onClose} />
+        <img src={closeBlack} className="cart__close-icon" alt="Close" onClick={onClose} />
       </div>
       <div className="cart__container">
         <div className="cart__item">
           <img className="cart__item-img" src={product}></img>
-          <p className="cart__item-name">Nike AirMax</p>
-          <div className="cart__button-container">
-            <button className="cart__button-item">-</button>
-            <button className="cart__button-item">+</button>
+          <div className="cart__item-info-container">
+            <p className="cart__item-name">Nike AirMax</p>
+            <div className="cart__button-container">
+              <button className="cart__button-item">-</button>
+              <input type="Number" value="2" name="quantity" required className="cart__input" />
+              <button className="cart__button-item">+</button>
+            </div>
           </div>
         </div>
         <div className="cart__item">
           <img className="cart__item-img" src={product}></img>
-          <p className="cart__item-name">Adidas Ultraboost</p>
-          <div className="cart__button-container">
-            <button className="cart__button-item">-</button>
-            <button className="cart__button-item">+</button>
+          <div className="cart__item-name-container">
+            <p className="cart__item-name">Adidas Ultraboost</p>
+            <div className="cart__button-container">
+              <button className="cart__button-item">-</button>
+              <input type="Number" value="1" name="quantity" required className="cart__input" />
+              <button className="cart__button-item">+</button>
+            </div>
           </div>
         </div>
       </div>
-      <button className="cart__button-total">Total</button>
+      <button type="submit" className="cart__button-total">PAGAR (300€)</button>
     </div>
   );
 };
