6db6bb30dffa96a529ba716982756e0d325d24fa
francesc <you@example.com>
2024-07-05 12:18:24 +0200

    added a stripe payment on checkout

--- src/hooks/StripePayment.tsx
+++ src/hooks/StripePayment.tsx
@@ -24,12 +24,45 @@ interface PaymentResult {
 }
 
 const useStripePayment = (): { processPayment: (formData: FormData) => Promise<PaymentResult>; error: string; } => {
-  const [error] = useState("");
+  const [error, setError] = useState("");
 
   const processPayment = async (formData: FormData): Promise<PaymentResult> => {
-    console.log("hola aquí introduciremos el pago de Stripe");
-    // Aquí ira la lógica para Stripe
-    return { success: true };
+    const URL_API_CHECKOUT_STRIPE = `${process.env.REACT_APP_API_URL as string}/stripe/checkout`;
+
+    const payload = {
+      paymentMethodId: "pm_card_visa",
+      amount: formData.price.toFixed(2),
+      name: `${formData.firstName} ${formData.lastName}`,
+      email: formData.email,
+      address: {
+        line1: formData.address,
+        city: formData.locality,
+        postal_code: formData.postalCode,
+        country: formData.country,
+      }
+    };
+
+    try {
+      const response = await fetch(URL_API_CHECKOUT_STRIPE, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        return { success: true, data };
+      } else {
+        setError(data.message);
+        return { success: false, error: data.message };
+      }
+    } catch (error) {
+      setError("Error en la transacción");
+      return { success: false, error: "Error en la transacción" };
+    }
   };
 
   return { processPayment, error };
